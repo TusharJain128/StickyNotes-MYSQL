27b3e04b2e81e496335e0eec62a56ba1da452570
TusharJain128 <tusharjain128@gmail.com>
2025-07-13 15:52:47 +0530

    fixed cors issue

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,17 +4,25 @@ const route = require("./routes/route");
 const cors = require("cors");
 
 const app = express();
+
+// Configure CORS properly
+const corsOptions = {
+  origin: [
+    'https://tushar-stickynotes.netlify.app',
+    'http://localhost:3000',
+    'http://localhost:3001'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 app.use("/", route);
-
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`server start on ${process.env.PORT || 3001}`);
